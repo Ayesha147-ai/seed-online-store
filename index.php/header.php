e5e12f910ed66973a27701917b223b2dd502373b
mariyam110 <mughalria89@gmail.com>
2026-09-10 18:48:46 +0500

    logo click

---
+++ index.php/header.php
@@ -0,0 +1,15 @@
+<?php
+// header.php
+?>
+<section id="header">
+    <a href="index.php" class="logo">
+        <i class="fas fa-seedling"></i>
+        <span>TrackSeed</span>
+    </a>
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="auth/login.php">Login</a></li>
+    </ul>
+</section>
